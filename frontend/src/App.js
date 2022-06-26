f894dc00f9ba7a1ac39f1446573ee6b06ab73d6b
Luciano Coelho <coelho.luciano@icloud.com>
2022-06-26 14:39:30 -0300

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.svg';
+
+function App() {
+  return (
+    <h1>Hello Rocketseat</h1>
+  );
+}
+
+export default App;
