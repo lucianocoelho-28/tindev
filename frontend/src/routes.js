dfcb2aba9e9ed733efc0c226ca8742a24741f2a2
Luciano Coelho <coelho.luciano@icloud.com>
2022-06-27 21:49:50 -0300

    frontend

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,11 +4,11 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Main from './pages/Main';
 
-export default function Routes() {
-  return (
-    <BrowserRouter>
-      <Route path="/"component={Login} />
-      <Route path="/main"component={Main} />
-    </BrowserRouter>
-  );
+export default function Routers(){
+    return (
+        <BrowserRouter>
+            <Route path="/" exact component={Login}/>
+            <Route path="/dev/:id" component={Main} />
+        </BrowserRouter>
+    );
 }
