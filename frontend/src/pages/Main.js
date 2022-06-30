2aeb9a0ef9b8f815c0aab6d9b1fb41870dca51cc
Luciano Coelho <coelho.luciano@icloud.com>
2022-06-30 17:17:16 -0300

    frontend

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Main.css';
 
 import api from '../services/api';
@@ -41,7 +42,9 @@ export default function Main({ match }) {
 
   return (
     <div className="main-container">
-      <img src={logo} alt="Tindev" />
+      <Link to='/'>
+        <img src={logo} alt="Tindev" />
+      </Link>
         { users.length > 0 ? (
           <ul>
                   {users.map(user => (
